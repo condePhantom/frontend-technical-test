fe2ccec4ef12cb0ce93fa4f3525dbbcd21970eb9
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-21 16:00:41 -0700

    feat: create empty page for home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import BaseLayout from "../components/layout/BaseLayout";
+import { Toolbar, Typography } from "@mui/material";
+const Home: FC = () => {
+  return (
+    <BaseLayout>
+      <h1>Home</h1>
+      Movies Series
+    </BaseLayout>
+  );
+};
+
+export default Home;
