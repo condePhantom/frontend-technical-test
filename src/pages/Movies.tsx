0b23891fecac22e88ef859c99f03afb0616eddd7
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-21 23:09:29 -0700

    feat: add movies and series pages in blank

---
+++ src/pages/Movies.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import BaseLayout from "../components/layout/BaseLayout";
+
+const Movies: FC = () => {
+  return (
+    <BaseLayout title={"Popular Movies"}>
+      <h1>Movies</h1>
+    </BaseLayout>
+  );
+};
+
+export default Movies;
---
+++ src/pages/Series.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import BaseLayout from "../components/layout/BaseLayout";
+
+const Series: FC = () => {
+  return (
+    <BaseLayout title={"Popular Series"}>
+      <h1>Movies</h1>
+    </BaseLayout>
+  );
+};
+
+export default Series;
