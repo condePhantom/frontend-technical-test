840ea7c997cae6171a053ac7311e903d57b86b75
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-21 22:58:48 -0700

    feat: integrate home categories component in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import BaseLayout from "../components/layout/BaseLayout";
-import { Toolbar, Typography } from "@mui/material";
+import HomeCategories from "../components/organisms/HomeCategories";
+
 const Home: FC = () => {
   return (
     <BaseLayout>
-      <h1>Home</h1>
-      Movies Series
+      <HomeCategories />
     </BaseLayout>
   );
 };
