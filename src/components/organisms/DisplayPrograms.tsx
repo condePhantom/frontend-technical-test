7b22bf30179d58b775bfaae4ffc120de892d2b8c
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-22 23:05:35 -0700

    feat: integrate ProgramModal to DisplayPrograms component

--- src/components/organisms/DisplayPrograms.tsx
+++ src/components/organisms/DisplayPrograms.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Box } from "@mui/material";
 import DisplayCardList from "../molecules/DisplayCardList";
+import ProgramModal from "../molecules/ProgramModal";
 import { Program } from "../../types/types";
 
 interface DisplayProgramsProps {
@@ -8,6 +9,14 @@ interface DisplayProgramsProps {
 }
 
 const DisplayPrograms: FC<DisplayProgramsProps> = ({ programs }) => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState<Program>();
+
+  const handleOpen = (prog: Program) => {
+    setSelected(prog);
+    setOpen(true);
+  };
+
   return (
     <Box
       display="flex"
@@ -15,7 +24,17 @@ const DisplayPrograms: FC<DisplayProgramsProps> = ({ programs }) => {
       flexWrap="wrap"
       sx={{ justifyContent: { xs: "center", md: "space-between" } }}
     >
-      <DisplayCardList programs={programs} />
+      <DisplayCardList programs={programs} onClick={handleOpen} />
+      {selected && (
+        <ProgramModal
+          program={selected}
+          open={open}
+          onClose={() => {
+            setOpen(false);
+            setSelected(undefined);
+          }}
+        />
+      )}
     </Box>
   );
 };
