19cd651819ece92aec7a0b7b2a06ed72772366dc
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-23 16:08:41 -0700

    feat: integrate ProgramPaginator to DisplayPrograms organism

--- src/components/organisms/DisplayPrograms.tsx
+++ src/components/organisms/DisplayPrograms.tsx
@@ -2,13 +2,15 @@ import { FC, useState } from "react";
 import { Box } from "@mui/material";
 import DisplayCardList from "../molecules/DisplayCardList";
 import ProgramModal from "../molecules/ProgramModal";
-import { Program } from "../../types/types";
+import ProgramPaginator from "..//atoms/ProgramPaginator";
+import { Program, ProgramPaginatorProps } from "../../types/types";
 
 interface DisplayProgramsProps {
   programs: Program[];
+  paginator: ProgramPaginatorProps;
 }
 
-const DisplayPrograms: FC<DisplayProgramsProps> = ({ programs }) => {
+const DisplayPrograms: FC<DisplayProgramsProps> = ({ programs, paginator }) => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState<Program>();
 
@@ -18,24 +20,27 @@ const DisplayPrograms: FC<DisplayProgramsProps> = ({ programs }) => {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="row"
-      flexWrap="wrap"
-      sx={{ justifyContent: { xs: "center", md: "space-between" } }}
-    >
-      <DisplayCardList programs={programs} onClick={handleOpen} />
-      {selected && (
-        <ProgramModal
-          program={selected}
-          open={open}
-          onClose={() => {
-            setOpen(false);
-            setSelected(undefined);
-          }}
-        />
-      )}
-    </Box>
+    <>
+      <Box
+        display="flex"
+        flexDirection="row"
+        flexWrap="wrap"
+        sx={{ justifyContent: { xs: "center", md: "space-between" } }}
+      >
+        <DisplayCardList programs={programs} onClick={handleOpen} />
+        {selected && (
+          <ProgramModal
+            program={selected}
+            open={open}
+            onClose={() => {
+              setOpen(false);
+              setSelected(undefined);
+            }}
+          />
+        )}
+      </Box>
+      {programs.length > 0 && <ProgramPaginator {...paginator} />}
+    </>
   );
 };
 
