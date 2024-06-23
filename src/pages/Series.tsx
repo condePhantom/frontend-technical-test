66032b44ff17381ec3cdf71be4d265c060cdd477
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-23 16:39:17 -0700

    feat: config pagination in series page

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,10 +1,50 @@
 import { FC } from "react";
 import BaseLayout from "../components/layout/BaseLayout";
+import DisplayPrograms from "../components/organisms/DisplayPrograms";
+import { SelectChangeEvent } from "@mui/material";
+import { ProgramPaginatorProps } from "../types/types";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../store";
+import { setCurrentPage, setSeriesPerPage } from "../store/seriesSlice";
 
 const Series: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const series = useSelector((state: RootState) => state.series.series);
+  const currentPage = useSelector(
+    (state: RootState) => state.series.currentPage
+  );
+
+  const seriesPerPage = useSelector(
+    (state: RootState) => state.series.seriesPerPage
+  );
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    dispatch(setCurrentPage(page));
+  };
+
+  const handleSeriesPerPageChange = (event: SelectChangeEvent) => {
+    dispatch(setSeriesPerPage(event.target.value as unknown as number));
+    dispatch(setCurrentPage(1));
+  };
+  const indexOfLastMovie = currentPage * seriesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - seriesPerPage;
+  const currentSeries = series.slice(indexOfFirstMovie, indexOfLastMovie);
+  const count = Math.ceil(series.length / seriesPerPage);
+
+  const paginator: ProgramPaginatorProps = {
+    currentPage,
+    programsPerPage: seriesPerPage,
+    handleProgramsPerPageChange: handleSeriesPerPageChange,
+    handlePageChange,
+    count,
+  };
+
   return (
     <BaseLayout title={"Popular Series"}>
-      <h1>Movies</h1>
+      <DisplayPrograms programs={currentSeries} paginator={paginator} />
     </BaseLayout>
   );
 };
