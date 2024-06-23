c796ee079f67f8d96661e8dd08190ecf825cf564
Cesar Perez <ing.cesarperz@gmail.com>
2024-06-23 16:38:45 -0700

    feat: config pagination in movies page

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,98 +1,49 @@
 import { FC } from "react";
 import BaseLayout from "../components/layout/BaseLayout";
-
 import DisplayPrograms from "../components/organisms/DisplayPrograms";
-import { Program } from "../types/types";
-const data: Program[] = [
-  {
-    title: "Rake",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    programType: "series",
-    images: {
-      "Poster Art": {
-        url: "https://streamcoimg-a.akamaihd.net/000/165/9/1659-PosterArt-b326059d852397768897083483b44324.jpeg",
-        width: 1000,
-        height: 1500,
-      },
-    },
-    releaseYear: 2010,
-  },
-  {
-    title: "30 Rock",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    programType: "series",
-    images: {
-      "Poster Art": {
-        url: "https://streamcoimg-a.akamaihd.net/000/532/30/53230-PosterArt-df844c976b27c518d83c2707e79b4685.jpeg",
-        width: 1000,
-        height: 1500,
-      },
-    },
-    releaseYear: 2006,
-  },
-  {
-    title: "The Resort",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    programType: "series",
-    images: {
-      "Poster Art": {
-        url: "https://streamcoimg-a.akamaihd.net/000/885/68/88568-PosterArt-a988d5aa0090e892f9015dd5ad17c0ac.jpg",
-        width: 1000,
-        height: 1500,
-      },
-    },
-    releaseYear: 2022,
-  },
-  {
-    title: "Wonder",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    programType: "movie",
-    images: {
-      "Poster Art": {
-        url: "https://streamcoimg-a.akamaihd.net/000/165/9396/1659396-PosterArt-14385926f542bb6cb6c1fc84d907a9b2.jpeg",
-        width: 1000,
-        height: 1500,
-      },
-    },
-    releaseYear: 2017,
-  },
-  {
-    title: "3:10 to Yuma",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    programType: "movie",
-    images: {
-      "Poster Art": {
-        url: "https://streamcoimg-a.akamaihd.net/000/293/8294/2938294-PosterArt-3138b2ec41be4d8837f5e34b747412ee.jpg",
-        width: 1000,
-        height: 1500,
-      },
-    },
-    releaseYear: 2007,
-  },
-  {
-    title: "Self/less",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    programType: "movie",
-    images: {
-      "Poster Art": {
-        url: "https://streamcoimg-a.akamaihd.net/000/103/5943/1035943-PosterArt-abb0a86a7b981aad0e1bcd9a85262bac.jpeg",
-        width: 1000,
-        height: 1500,
-      },
-    },
-    releaseYear: 2015,
-  },
-];
+import { SelectChangeEvent } from "@mui/material";
+import { ProgramPaginatorProps } from "../types/types";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../store";
+import { setCurrentPage, setMoviesPerPage } from "../store/moviesSlice";
+
 const Movies: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const currentPage = useSelector(
+    (state: RootState) => state.movies.currentPage
+  );
+  const moviesPerPage = useSelector(
+    (state: RootState) => state.movies.moviesPerPage
+  );
+  const movies = useSelector((state: RootState) => state.movies.movies);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    dispatch(setCurrentPage(page));
+  };
+
+  const handleMoviesPerPageChange = (event: SelectChangeEvent) => {
+    dispatch(setMoviesPerPage(event.target.value as unknown as number));
+    dispatch(setCurrentPage(1));
+  };
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
+  const count = Math.ceil(movies.length / moviesPerPage);
+
+  const paginator: ProgramPaginatorProps = {
+    currentPage,
+    programsPerPage: moviesPerPage,
+    handleProgramsPerPageChange: handleMoviesPerPageChange,
+    handlePageChange,
+    count,
+  };
+
   return (
     <BaseLayout title={"Popular Movies"}>
-      <DisplayPrograms programs={data} />
+      <DisplayPrograms programs={currentMovies} paginator={paginator} />
     </BaseLayout>
   );
 };
